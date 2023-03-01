df564a90c0c1e619dd0f20d107da81f1b5268ecb
Franco Petraglia <frannpetragliagmail.com>
2023-03-01 18:21:15 -0300

    primer commit

---
+++ funciones.js
@@ -0,0 +1,12 @@
+
+let anteriorBtn = document.getElementById("div1");
+let siguienteBtn = document.getElementById("div2");
+let imgchange = document.getElementById("imgch");
+
+anteriorBtn.onclick = function() {
+    imgchange.src= "Multimedia/bonucci.jpg"
+}
+
+siguienteBtn.onclick = function(){
+    imgchange.src= "Multimedia/leoB.jpg"
+}
